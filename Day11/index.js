e003cba4640cc81f0a51b249661b5a9f52ac3612
goswami889 <rounakgoswami29@gmail.com>
2025-01-11 15:37:08 +0530

    initial masseage

---
+++ Day11/index.js
@@ -0,0 +1,45 @@
+// console.log("namaste duniya version 2");
+   
+// let a=6;
+// console.log(a);
+
+// let lastName ="rounak"
+// console.log(lastName);
+
+// lastName ="3"
+// console.log(lastName);
+
+// console.log(2>3);
+// // condition chaekd 
+// let age=17;
+// let status =(age>=18)?"i can vote":"can not vote";
+// console.log(status);
+// // if else stament
+// let marks=100;
+// if(marks>=80){
+//     console.log("a");
+// }
+// else if(marks>=70){
+//     console.log("b");
+// }
+// else{
+//     console.log("c");
+// }
+// // for loop
+// for(let i=0;i<5;i++){
+//     console.log(i);
+// }
+// // while loop
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+
+
+// do while loop
+let x=0;
+do{
+    console.log("y");
+
+}while(x<10)
+    
